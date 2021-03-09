3b4bfc73008e6c36abf4714b3d3414728a8370f3
Nicolas <nicolas.soares.gomes@gmail.com>
2021-03-08 21:35:32 -0300

    primeiro html com express

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+const express = require('express')
+const path = require('path')
+const app = express()
+
+let todosPokemons = []
+
+/**axios.get(' https://pokeapi.co/api/v2/pokemon?offset=1&limit=1118').then((response) => {
+    console.log(response.data)
+    let {results} = response.data
+    for(pokemons of results){
+        todosPokemons.push(pokemons.name)
+        console.log(todosPokemons)
+    }
+})
+*/
+
+
+app.listen(85, () => {
+    console.log("Server is running")
+})
+
+/** 
+app.get('/pokemons', (req, res) => {
+    const {nome} =  req.query
+    let listaRetorno = todosPokemons.filter(i => i.includes(nome || ""))
+    res.json(listaRetorno)
+})
+*/
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
